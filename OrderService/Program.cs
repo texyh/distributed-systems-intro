6c1419a8105d3f9a55736ef0633e53fcab229bb7
texyh <onwuzulikee1@gmail.com>
2018-08-23 11:27:21 +0100

    initial commit

---
+++ OrderService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OrderService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
